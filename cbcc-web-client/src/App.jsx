61cc78c187a52dd4cd9bb17946e33c0aecd51ee6
Yonas <yonas.alluniverse@gmail.com>
2022-10-19 22:47:42 -0300

    create project

---
+++ cbcc-web-client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+//Components
+import Home from './components/Home.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
